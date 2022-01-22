0f3358b791ad0635bd7e0ad8d51dca57a0933b84
James Bistis <james.bistis@icloud.com>
2022-01-22 12:51:14 -0500

    add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'form-container-issue';
-  tableConfigData: any;
+  shiftBreakData: any;
 
   constructor() {
-    this.tableConfigData = {
-      name: 'amount',
-      selector: 'number',
-      validators: [
+
+    const shiftBreaks = [
+      { amount: 1, breakId: 'm2g2gvNBAGlAgsXsmycp' },
+      { amount: 3, breakId: '9Hjp3MheJLY3qhftSZvz' },
+    ];
+    const shiftBreaksObservable = of(shiftBreaks);
+
+    this.shiftBreakData = {
+      sourceObservable: shiftBreaksObservable,
+      inputType: [
         {
-          name: 'min',
-          value: 1,
-          class: 'builtin',
-          type: 'sync',
-          trigger: 'onWrite'
-        }
+          name: 'amount',
+          selector: 'number',
+          validators: [
+            { name: 'min', value: 1, class: 'builtin', type: 'sync', trigger: 'onWrite' },
+          ]
+        },
       ]
     }
+
   }
 
   ngOnInit() {
-    console.log(this.tableConfigData)
+    console.log(this.shiftBreakData);
   }
+
+  updateBreaks(shiftBreaks: any) {
+  }
+
 
 }
