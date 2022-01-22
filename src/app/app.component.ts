6091b05925f1d015d8d948c760925f8dcb2e3254
James Bistis <james.bistis@icloud.com>
2022-01-22 12:26:28 -0500

    add tableConfigData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'form-container-issue';
+  tableConfigData: any;
+
+  constructor() {
+    this.tableConfigData = {
+      name: 'amount',
+      selector: 'number',
+      validators: [
+        {
+          name: 'min',
+          value: 1,
+          class: 'builtin',
+          type: 'sync',
+          trigger: 'onWrite'
+        }
+      ]
+    }
+  }
+
+  ngOnInit() {
+    console.log(this.tableConfigData)
+  }
+
 }
